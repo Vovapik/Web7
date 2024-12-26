15df10038bdb674614947d0da73dba9cf68942a1
Vovapik <140977092+Vovapik@users.noreply.github.com>
2024-12-26 11:10:42 +0200

    Update save_event.php

--- save_event.php
+++ save_event.php
@@ -1,4 +1,7 @@
 // save_event.php - Save immediate event to the database
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 require_once 'db.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Process the incoming POST data
